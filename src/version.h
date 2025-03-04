8bb9d716901f68bf8c59cd3b449a65015bd45e2a
Aegisum <aegisumcoin@gmail.com>
2025-03-03 23:49:11 -0500

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,37 +9,37 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70022;
+static const int PROTOCOL_VERSION = 70023;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70022;
+static const int MIN_PEER_PROTO_VERSION = 70023;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 70022;
+static const int BIP0031_VERSION = 70023;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70022;
+static const int NO_BLOOM_VERSION = 70023;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70022;
+static const int SENDHEADERS_VERSION = 70023;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 70022;
+static const int FEEFILTER_VERSION = 70023;
 
 //! short-id-based block download starts with this version
-static const int SHORT_IDS_BLOCKS_VERSION = 70022;
+static const int SHORT_IDS_BLOCKS_VERSION = 70023;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70015;
+static const int INVALID_CB_NO_BAN_VERSION = 70023;
 
 //! "wtxidrelay" command for wtxid-based relay starts with this version
 static const int WTXID_RELAY_VERSION = 70016;
 
 //! "mwebheader" command for light client MWEB support starts with this version
-static const int MWEB_SYNC_VERSION = 70022;
+static const int MWEB_SYNC_VERSION = 70023;
 
 // Make sure that none of the values above collide with
 // `SERIALIZE_TRANSACTION_NO_WITNESS` or `ADDRV2_FORMAT`.
